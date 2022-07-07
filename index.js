762e51086f0c7dadd66954f736cacf38afd0f1a5
AdanGarciaCPU <developingadan@gmail.com>
2022-07-07 02:15:38 -0500

    added more functionality

--- index.js
+++ index.js
@@ -8,23 +8,22 @@ async function example() {
     await driver.get("https://www.skatewarehouse.com/")
 
 
-    // get Daily Dose product Title
+    // get Daily Dose product Title & check if includes "deck" 
 
-    // const dailyDoseProduct = document.querySelector('.daily_dose').textContent
-
-    // const links = dailyDoseProduct.querySelectorAll('a')
-
+    const dailyDoseProduct = document.querySelector('.daily_dose').textContent.toLowerCase();
     // console.log(dailyDoseProduct)
-    
-    // dailyDoseProduct.includes("deck") ? true : false;
+
+    // try to extract all the values that read like "/n" and that other one (/t) I believe
+    dailyDoseProduct.includes("deck") ? console.log(`it's a deck! it's a ${dailyDoseProduct}`) : console.log(`it is not a deck :( ${dailyDoseProduct[0]}`);
 
     // get price value from Daily Dose
     let dailyDosePrice = document.querySelector('.price').textContent
     let converted = dailyDosePrice.slice(1)
-    console.log(converted)
+    // console.log(` Daily dose item price is ${converted}`)
 
-    converted < 35 ? alert(true) : alert(false);
+    converted < 35 ? console.log(`Daily Dose item price is good -- ${converted}`) : console.log(`Daily Dose item price TOO HIGH -- ${converted}`); // wrote shorthand way
 
+    
     // if (converted > 35) {
     //     alert("true")
     // } else {
